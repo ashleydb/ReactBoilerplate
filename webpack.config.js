f84092cedc172630c2ff39177dfac09a834d4dc6
Bennett, Ashley <ABennett@ea.com>
2017-01-23 13:02:53 -0800

    Adding image and foundation-icon support
    Fonts are not yet working.

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,8 @@ module.exports = {
     root: __dirname,
     modulesDirectories: [
       'node_modules',
-      './app/components/',
-      './app/api/'
+      './app/components',
+      './app/api'
     ],
     alias: {
       applicationStyles: 'app/styles/app.scss'
@@ -52,12 +52,25 @@ module.exports = {
       {
         test: /\.scss$/,
         loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
       }
     ]
   },
   sassLoader: {
     includePaths: [
-      path.resolve(__dirname, './node_modules/foundation-sites/scss')
+      path.resolve(__dirname, './node_modules/foundation-sites/scss'),
+      path.resolve(__dirname, './node_modules/foundation-icons')
     ]
   },
   //'eval-source-map' lets us debug the code as written, rather than in bundle.js.
